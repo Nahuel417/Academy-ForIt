f61aed6caf04cb02e67f0159c1381100a6c764c8
Nahuel Banco <146679252+Nahuel417@users.noreply.github.com>
2025-10-17 14:05:01 -0300

    feat: add addMovement.ts

--- domain/src/services/cashRegister-service.ts
+++ domain/src/services/cashRegister-service.ts
@@ -1,6 +1,10 @@
+import type { CashRegisterMovement } from '@domain/entities/cashRegisterMovement.js';
 import type { CashRegister } from '../entities/cashRegister.js';
 import type { Service } from '../utils/types/service.js';
 
 export interface CashRegisterService extends Service<CashRegister> {
     findByDate(date: Date): Promise<CashRegister | undefined>;
+    addMovement?(registerId: string, movement: CashRegisterMovement): Promise<void>;
+    removeMovement?(registerId: string, description: string): Promise<void>;
+    updateMovement?(registerId: string, movement: CashRegisterMovement): Promise<void>;
 }
