4c3e07d23f38cb8cc7c744f9b474381b9754deef
Nahuel Banco <146679252+Nahuel417@users.noreply.github.com>
2025-10-22 23:43:19 -0300

    feat: add entry point main

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,7 +1,9 @@
-import { app } from './app.js';
+// @ts-ignore
+import { app } from '@backend/app.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`📖 Health check: http://localhost:${PORT}/health`);
 });
